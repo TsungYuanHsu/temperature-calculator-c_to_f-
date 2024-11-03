c883fe0a2a4c0156ed4b8f4a1c46f8d609fbfa65
Johnson Hsu <specialline4107@gmail.com>
2024-11-03 21:48:50 +0800

    Add new file

---
+++ transfer_celsius_to_fahrenheit.py
@@ -0,0 +1,7 @@
+# This code is to transfer celsius to fahrenheit
+
+celsius = float(input('Please input celsius temperature: '))
+
+fahrenheit = celsius * 9 / 5 + 32
+
+print('The fahrenheit temperature of', fahrenheit)
